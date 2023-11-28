0805bb2706790f2453e8f079affcae92c7d6976f
unknown <2678557169@qq.com>
2023-11-28 17:10:18 +0800

    ECS framework

---
+++ Sources/src/Core/ECS/Level.cpp
@@ -0,0 +1,47 @@
+#pragma once
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include "ObjectIDAllocator.h"
+
+namespace CSEditor::Core{
+
+class GObject;
+class ObjectInstanceRes;
+
+using LevelObjectsMap = std::unordered_map<GObjectID, std::shared_ptr<GObject>>;
+
+/// The main class to manage all game objects
+class Level
+{
+public:
+    virtual ~Level(){};
+
+    bool load(const std::string& level_res_url);
+    void unload();
+
+    bool save();
+
+    void tick(float delta_time);
+
+    const std::string& getLevelResUrl() const { return m_level_res_url; }
+
+    const LevelObjectsMap& getAllGObjects() const { return m_gobjects; }
+
+    std::weak_ptr<GObject>   getGObjectByID(GObjectID go_id) const;
+    
+
+    GObjectID createObject(const ObjectInstanceRes& object_instance_res);
+    void deleteGObjectByID(GObjectID go_id);
+
+protected:
+    void clear();
+
+    bool        m_is_loaded {false};
+    std::string m_level_res_url;
+
+    // all game objects in this level, key: object id, value: object instance
+    LevelObjectsMap m_gobjects;
+};
+
+}
